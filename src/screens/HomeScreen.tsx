aff0f1432308ea0e19019438ef24f9f1568ea60f
Alyfaraj <afarag840@gmail.com>
2022-06-27 12:21:30 +0200

    feat(news):add loader to home

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,16 +1,17 @@
-import { Alert, StyleSheet, Text, View } from 'react-native'
+import { ActivityIndicator, Alert, StyleSheet, Text, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { axiosApi } from '../network'
 import axios from 'axios'
 import { Article } from '../types'
 import NewsList from '../components/NewsList'
 import SearchInput from '../components/SearchInput'
+import Colors from '../themes/Colors'
 
 const HomeScreen = () => {
     const [news, setNews] = useState<Article[]>([])
     const [searchword, setSearchword] = useState<string>('')
+    const [loading, setLoading] = useState<boolean>(false)
 
-   
 
     useEffect(() => {
         getAllNews()
@@ -18,6 +19,7 @@ const HomeScreen = () => {
 
 
     const getAllNews = (): void => {
+        setLoading(true)
         axiosApi.get(`/top-headlines`, {
             params: {
                 q: searchword ?? '',
@@ -25,21 +27,26 @@ const HomeScreen = () => {
             }
         })
             .then(response => {
+                setLoading(false)
                 console.log(response.data)
                 const articles = response.data?.articles
                 if (articles) {
                     setNews(articles)
                 }
             })
-            .catch(err => Alert.alert(err.response.data?.message))
+            .catch(err => {
+                setLoading(false)
+                Alert.alert(err.response.data?.message)
+            })
     }
 
 
 
     return (
-        <View style={styles.container} >
+        <View style={styles.container}>
             <SearchInput onChangeText={setSearchword} />
-            <NewsList data={news} loadMore={() => { }} />
+            {loading ? <ActivityIndicator size='large' color={Colors.Gray} />
+                : <NewsList data={news} loadMore={() => { }} />}
         </View>
     )
 }
@@ -50,5 +57,8 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'white'
+    },
+    notFound: {
+
     }
 })
